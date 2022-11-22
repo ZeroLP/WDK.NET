ac840d7e6e2bba8f755ea1673a5d7d275888d905
Zero <24566018+ZeroLP@users.noreply.github.com>
2022-11-22 16:59:30 +1100

    Added baseplate for MS official sample drivers.

---
+++ WDK.NET/MS Official Driver Samples/Moufiltr/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime;
+using System.Runtime.InteropServices;
+
+using WDK.NET;
+using WDK.NET.Kernel;
+using WDK.NET.Enums;
+using WDK.NET.SafeHelpers;
+using WDK.NET.CStd;
+
+namespace Moufiltr
+{
+    internal unsafe class Program
+    {
+        //Dummy main entrypoint method for CSC
+        static void Main() { }
+
+        //Dummy winmain method for linker
+        [RuntimeExport("wmainCRTStartup")]
+        static void wmainCRTStartup() { }
+
+        [RuntimeExport("DriverEntry")]
+        static NTSTATUS DriverEntry()
+        {
+            return NTSTATUS.Success;
+        }
+    }
+}
